2f3b1057a99f868a74603a94a1a7bc2caffadba5
Pedro R. Caetano <prcaetano@openmailbox.org>
2017-02-08 20:36:36 -0200

    renovação ocorre apenas se a data de entrega de algum livro for hoje ou flag RENOVA_SEMPRE é verdadeira.

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import os
 import logging
 import yaml
 
-from datetime import date, datetime
+from datetime import date
 
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
@@ -16,7 +16,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 CONFIG_DIR = os.path.join(os.environ['HOME'], '.sbu-bot/')
 CONFIG_FILE = os.path.join(CONFIG_DIR, "config.yml")
-DEBUG = True
+RODAR_SEMPRE = False
 
 logging.basicConfig(
     filename=os.path.join(CONFIG_DIR, 'log.log'), level=logging.INFO,
@@ -66,11 +66,62 @@ def login(email, senha, firefox):
         send_button.click()
         WebDriverWait(firefox, 30).until(EC.visibility_of_element_located((By.CLASS_NAME, 'justificado')))
     except Exception, err:
-        error_message = "deu ruim"
+        error_message = "Erro no login. Verifique usuário e senha."
         print error_message
         logging.exception(error_message)
         raise
     return
+
+
+def renova(firefox):
+    firefox.get('http://acervus.unicamp.br/')
+    firefox.implicitly_wait(10)
+    WebDriverWait(firefox, 60).until(EC.visibility_of_element_located((By.ID, 'mainFrame')))
+    firefox.switch_to_frame(firefox.find_element_by_id('mainFrame'))
+    try:
+        # seleciona e clica no menu "Serviços"
+        firefox.find_element_by_xpath(
+            '//a[contains(text(), "Serviços")]').click()
+        firefox.implicitly_wait(30)
+        # selecion e clica "Circ./Renovação"
+        firefox.find_element_by_xpath(
+            '//a[contains(text(), "Circ./Renovação")]').click()
+
+        WebDriverWait(
+            firefox, 30).until(
+            EC.visibility_of_element_located(
+                (By.LINK_TEXT, 'Renovar itens selecionados')))
+
+        tabela = firefox.find_elements_by_css_selector(
+            'table.tab_circulacoes')[0]
+        livros = tabela.find_elements_by_css_selector('tr')[1:]
+
+        # renovar = True só se há alguma entrega para hoje
+        renovar = False
+        hoje = date.today()
+        for livro in livros:
+            data_devolucao_str = livro.find_elements_by_css_selector(
+                'td')[-1].text
+            data_devolucao = datetime.strptime(
+                data_devolucao_str, '%d/%m/%y').date()
+
+            if data_devolucao == hoje:
+                renovar = True
+                livro.find_element_by_tag_name('input').click()
+
+        if renovar:
+            firefox.find_element_by_link_text(
+                'Renovar itens selecionados').click()
+            firefox.implicitly_wait(30)
+            WebDriverWait(
+                firefox, 60).until(
+                EC.visibility_of_element_located((By.ID, 'mainFrame')))
+            logging.info("Renovado.")
+    except Exception, err:
+        error_message = "Erro ao renovar."
+        print error_message
+        logging.exception(error_message)
+        raise
 
 
 config_dict = load_config()
@@ -83,7 +134,7 @@ except Exception, err:
     logging.exception(error_message)
     raise
 lastrun = config_dict.get('lastrun')
-if lastrun == date.today() and DEBUG is False:
+if lastrun == date.today() and not RODAR_SEMPRE:
     exit()
 else:
     config_dict['lastrun'] = date.today()
@@ -97,55 +148,6 @@ except WebDriverException, err:
     print error_message
     logging.exception(error_message)
     raise
-
 login(email, senha, firefox)
-firefox.get('http://acervus.unicamp.br/')
-firefox.implicitly_wait(10)
-WebDriverWait(firefox, 60).until(EC.visibility_of_element_located((By.ID, 'mainFrame')))
-firefox.switch_to_frame(firefox.find_element_by_id('mainFrame'))
-
-try:
-    # seleciona e clica no menu "Serviços"
-    firefox.find_element_by_xpath(
-        '//a[contains(text(), "Serviços")]').click()
-    firefox.implicitly_wait(30)
-    # selecion e clica "Circ./Renovação"
-    firefox.find_element_by_xpath(
-        '//a[contains(text(), "Circ./Renovação")]').click()
-
-    WebDriverWait(
-        firefox, 30).until(
-        EC.visibility_of_element_located(
-            (By.LINK_TEXT, 'Renovar itens selecionados')))
-
-    tabela = firefox.find_elements_by_css_selector(
-        'table.tab_circulacoes')[0]
-    livros = tabela.find_elements_by_css_selector('tr')[1:]
-
-    # renovar = True só se há alguma entrega para hoje
-    renovar = False
-    hoje = date.today()
-    for livro in livros:
-        data_devolucao_str = livro.find_elements_by_css_selector(
-            'td')[-1].text
-        data_devolucao = datetime.strptime(
-            data_devolucao_str, '%d/%m/%y').date()
-
-        if data_devolucao == hoje:
-            renovar = True
-            livro.find_element_by_tag_name('input').click()
-
-    if renovar:
-        firefox.find_element_by_link_text(
-            'Renovar itens selecionados').click()
-        firefox.implicitly_wait(30)
-        WebDriverWait(
-            firefox, 60).until(
-            EC.visibility_of_element_located((By.ID, 'mainFrame')))
-        logging.info("Renovado.")
-except Exception, err:
-    error_message = "bosta"
-    print error_message
-    logging.exception(error_message)
-    raise
+renova(firefox)
 #firefox.close()
