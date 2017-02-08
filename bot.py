08d16a4bf4628d2cf874f073a85e529eddfbaff4
Pedro R. Caetano <prcaetano@openmailbox.org>
2017-02-08 19:09:09 -0200

    bot agora lê email e senha do arquivo de configuração config.yml (por padrão em /home/pedro/.sbu-bot/config.yml)

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
+import yaml
 
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
@@ -10,7 +11,26 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-os.system("./download_gecko2.sh")
+CONFIG_DIR = os.path.join(os.environ['HOME'], '.sbu-bot/')
+CONFIG_FILE = os.path.join(CONFIG_DIR, "config.yml")
+
+
+def load_config():
+    """
+    loads config dictionary containing keys "email" and "senha"
+    from config.yml file
+    """
+    try:
+        with open(CONFIG_FILE, "r") as config_file:
+            config = yaml.load(config_file)
+        config_dict = config['sbu-bot']
+    except Exception, err:
+        print "Erro na leitura de configuração:"
+        raise
+    return config_dict
+
+
+os.system("./download_gecko.sh")
 try:
     firefox = webdriver.Firefox(executable_path='./geckodriver')
 except WebDriverException, err:
@@ -18,15 +38,15 @@ except WebDriverException, err:
     raise
 
 
-def login():
+def login(email, senha):
     firefox.get('http://acervus.unicamp.br/asp/login.asp?modo_busca=rapida&content=mensagens&iBanner=0&iEscondeMenu=0&iSomenteLegislacao=0&iIdioma=0')
     firefox.implicitly_wait(10)
     WebDriverWait(firefox, 60).until(EC.visibility_of_element_located((By.ID, 'button1')))
     try:
         input_login = firefox.find_element_by_name("codigo")
-        input_login.send_keys('email')
+        input_login.send_keys(email)
         input_pwd = firefox.find_element_by_name("senha")
-        input_pwd.send_keys('senha')
+        input_pwd.send_keys(senha)
         send_button = firefox.find_element_by_id("button1")
         send_button.click()
         WebDriverWait(firefox, 30).until(EC.visibility_of_element_located((By.CLASS_NAME, 'justificado')))
@@ -35,7 +55,12 @@ def login():
         print Exception, err
     return
 
-login()
+
+config_dict = load_config()
+email = config_dict['email']
+senha = config_dict['senha']
+
+login(email, senha)
 firefox.get('http://acervus.unicamp.br/')
 firefox.implicitly_wait(10)
 WebDriverWait(firefox, 60).until(EC.visibility_of_element_located((By.ID, 'mainFrame')))
