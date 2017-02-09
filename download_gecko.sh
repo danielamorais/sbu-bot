84327a0727432776aaeeac8587f00dea4bcf69dd
Pedro R. Caetano <prcaetano@openmailbox.org>
2017-02-09 03:14:00 -0200

    Criado script de instalação install.sh.

--- download_gecko.sh
+++ download_gecko.sh
@@ -2,8 +2,8 @@
 GECKO_VERSION='v0.14.0'
 GECKO_PLATFORM='linux64'
 
-GECKO_FILE="geckodriver-"$GECKO_VERSION"-"$GECKO_PLATFORM".tar.gz"
-GECKO_URL="https://github.com/mozilla/geckodriver/releases/download/"$GECKO_VERSION"/"$GECKO_FILE
+GECKO_FILE="geckodriver-$GECKO_VERSION-$GECKO_PLATFORM.tar.gz"
+GECKO_URL="https://github.com/mozilla/geckodriver/releases/download/$GECKO_VERSION/$GECKO_FILE"
 
 if [[ ! -e ./geckodriver ]]
 then
