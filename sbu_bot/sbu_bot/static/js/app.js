d93cf71aa64273dc75757c8c64e141e1b9da94f5
Daniela Morais <dani.m@hotmail.co.uk>
2017-02-10 00:55:07 -0200

    Mensagem de erro e arquivos JS
    Verificacao se e-mail é valido e exibicao de mensagem de erro caso seja invalido.

--- sbu_bot/sbu_bot/static/js/app.js
+++ sbu_bot/sbu_bot/static/js/app.js
@@ -1,1 +1,13 @@
-$(document).foundation()
+//TODO: Enviar request se email valido
+function validarEmail() {
+    var email = document.getElementById("email").value;
+    var div = document.getElementById("erroEmail");
+    console.log("O email eh " + email);
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    regex.test(email) ? console.log("ok") : div.style.display = "block";
+}
+
+function esconderDiv() {
+    var div = document.getElementById("erroEmail");
+    div.style.display = "none";
+}
